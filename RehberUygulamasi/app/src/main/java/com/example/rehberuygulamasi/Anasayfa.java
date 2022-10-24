43c2e4fc0497eab5416e6004c77e1de75a760d10
Ömer Faruk Ercivan <ofar38@gmail.com>
2022-10-24 12:32:04 +0300

    yorum yaptım

--- RehberUygulamasi/app/src/main/java/com/example/rehberuygulamasi/Anasayfa.java
+++ RehberUygulamasi/app/src/main/java/com/example/rehberuygulamasi/Anasayfa.java
@@ -19,7 +19,7 @@ public class Anasayfa extends AppCompatActivity {
 
     TextView isimtv, emailtv;
     Button btnCikis, btnKategoriler, btnFavoriler;
-    //selam ben ömer
+    //selam ben ömer asdasd
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
